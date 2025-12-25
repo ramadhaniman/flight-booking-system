61f78d8a47e6eac8b7fa9682ea7f0d044e92f73a
Ramadhan Iman <rama.ramadhaniman@gmail.com>
2025-12-25 13:48:43 +0700

    Menu Icon change

--- app/Filament/Resources/Airports/AirportResource.php
+++ app/Filament/Resources/Airports/AirportResource.php
@@ -22,7 +22,7 @@ class AirportResource extends Resource
 {
     protected static ?string $model = Airport::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedMap;
 
     protected static ?string $recordTitleAttribute = 'name';
 
